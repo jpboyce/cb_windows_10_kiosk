85a6be6d456856f79e90289a57ee6dbd64712291
Jesse Boyce <jesse@jpboyce.org>
2018-02-04 08:05:37 +1000

    first pass

--- recipes/cis-5-system-services.rb
+++ recipes/cis-5-system-services.rb
@@ -3,5 +3,28 @@
 # Service template to stop and disable a service
 #windows_service 'servicename' do 
 #    action [:stop, :disable]
-#end    
+#end
 
+# if CIS Level 1 is set to yes, then we apply the items
+if node['cis']['level']['1'] == 'yes'
+    # apply level 1 items
+    node['cis-5']['level1']['services'].each do |i|
+        windows_service i do
+            action [:stop, :disable]
+        end
+    end
+else
+# do nothing
+end
+
+# if CIS Level 2 is set to yes, then we apply the items
+if node['cis']['level']['2'] == 'yes'
+# apply level 2 items
+    node['cis-5']['level1']['services'].each do |i|
+        windows_service i do
+            action [:stop, :disable]
+        end
+    end
+else
+# do nothing
+end
