53c3d7b35957bf32687beb1c297391f30c39c1db
Jesse Boyce <jesse@jpboyce.org>
2018-02-04 09:01:19 +1000

    added check to see if a service exists before attempting to stop/disable it

--- recipes/cis-5-system-services.rb
+++ recipes/cis-5-system-services.rb
@@ -3,14 +3,17 @@
 # Service template to stop and disable a service
 # windows_service 'servicename' do
 #    action [:stop, :disable]
-# end
+# xend
 
 # if CIS Level 1 is set to yes, then we apply the items
 if node['cis']['level']['1'] == 'yes'
     # apply level 1 items
     node['cis-5']['level1']['services'].each do |i|
-        windows_service i do
-            action [:stop, :disable]
+        if ::Win32::Service.exists?(i)
+            # check if the service actually exists first so we don't generate errors
+            windows_service i do
+                action [:stop, :disable]
+            end
         end
     end
 end
@@ -19,8 +22,11 @@ end
 if node['cis']['level']['2'] == 'yes'
     # apply level 2 items
     node['cis-5']['level1']['services'].each do |i|
-        windows_service i do
-            action [:stop, :disable]
+        if ::Win32::Service.exists?(i)
+            # check if the service actually exists first so we don't generate errors
+            windows_service i do
+                action [:stop, :disable]
+            end
         end
     end
 end
