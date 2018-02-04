c0e348d5c629e2dbea729a545c7f20590272d09f
Jesse Boyce <jesse@jpboyce.org>
2018-02-04 11:58:14 +1000

    Added powershell_script to check for dependent services and stop them so the current service can be properly stopped and disabled

--- recipes/cis-5-system-services.rb
+++ recipes/cis-5-system-services.rb
@@ -11,6 +11,23 @@ if node['cis']['level']['1'] == 'yes'
     node['cis-5']['level1']['services'].each do |i|
         if ::Win32::Service.exists?(i)
             # check if the service actually exists first so we don't generate errors
+            Chef::Log.warn("Checking if service #{i} has any dependencies")
+            # Have to do this code because it seems that Chef can only see dependencies in one direction, to the parents (items that the current one is dependent on)
+            # What we really need are the children (items that are dependent on the current one) so we can turn them off first
+            powershell_script 'DependencyCheck' do
+                code <<-EOH
+                    Start-Transcript -Verbose
+                    $serviceList = (Get-Service -Name #{i} | Select DependentServices).DependentServices
+                    if( ($serviceList.count) -gt 0) {
+                        Write-Warning "Dependent services have been found, going to brute force stop them..."
+                        foreach($item in $serviceList){
+                            Write-Warning "Attempting to stop service $($item.Name)"
+                            Get-Service $item.Name | Stop-Service -Force -Verbose
+                        }
+                    }
+                    Stop-Transcript -Verbose
+                EOH
+            end
             windows_service i do
                 action [:stop, :disable]
             end
