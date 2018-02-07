171879dfe3d2cc59cfc4db874c1a4e0d6f0f4d18
Jesse Boyce <jesse@jpboyce.org>
2018-02-07 20:08:31 +1000

    Added comment about foodcritic checks

--- recipes/cis-19-1-admin-templates-user-control-panel.rb
+++ recipes/cis-19-1-admin-templates-user-control-panel.rb
@@ -1,4 +1,5 @@
 # Windows 10 CIS Benchmark Items - Section 19.1 - Administrative Templates (User), Control Panel
+# Foodcritic will complain about this rule, but this layout is more aligned with what we want anyway
 
 # Ensure 'Enable screen saver' is set to 'Enabled'
 registry_key 'HKEY_USERS\[USER SID]\SOFTWARE\Policies\Microsoft\Windows\Control Panel\Desktop' do
