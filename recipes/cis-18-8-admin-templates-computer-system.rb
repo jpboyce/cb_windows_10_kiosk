c27ff4b86efec70c1d9e42dd1803dbba114ffedd
Jesse Boyce <jesse@jpboyce.org>
2018-02-07 21:34:21 +1000

    Commented out items that relate to node being a laptop until I can write a check for a node being a laptop or not

--- recipes/cis-18-8-admin-templates-computer-system.rb
+++ recipes/cis-18-8-admin-templates-computer-system.rb
@@ -1,7 +1,7 @@
 # Windows 10 CIS Benchmark Items - Section 18.8 - Administrative Templates (Computer), System
 
 # Ensure 'Include command line in process creation events' is set to 'Disabled'
-registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\ Audit' do
+registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\Audit' do
    values [{
      name: 'ProcessCreationIncludeCmdLine_Enabled',
      type: :dword,
@@ -22,7 +22,7 @@ end
 
 # Ensure 'Prevent installation of devices that match any of these device IDs' is set to 'Enabled'
 # BitLocker Rule
-registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrict ions' do
+registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions' do
    values [{
      name: 'DenyDeviceIDs',
      type: :dword,
@@ -34,7 +34,7 @@ end
 
 # Ensure 'Prevent installation of devices that match any of these device IDs: Prevent installation of devices that match any of these device IDs' is set to 'PCI\CC_0C0A'
 # BitLocker Rule
-registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrict ions\DenyDeviceIDs' do
+registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs' do
    values [{
      name: '1',
      type: :dword,
@@ -46,7 +46,7 @@ end
 
 # Ensure 'Prevent installation of devices that match any of these device IDs: Also apply to matching devices that are already installed.' is set to 'True
 # BitLocker Rule
-registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrict ions' do
+registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions' do
    values [{
      name: 'DenyDeviceIDsRetroactive',
      type: :dword,
@@ -58,7 +58,7 @@ end
 
 # Ensure 'Prevent installation of devices using drivers that match these device setup classes' is set to 'Enabled'
 # BitLocker Rule
-registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrict ions' do
+registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions' do
    values [{
      name: 'DenyDeviceClasses',
      type: :dword,
@@ -70,7 +70,7 @@ end
 
 # Ensure 'Prevent installation of devices using drivers that match these device setup classes: Prevent installation of devices using drivers for these device setup' is set to '{d48179be-ec20-11d1-b6b800c04fa372a7}'
 # BitLocker Rule
-registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrict ions\DenyDeviceClasses' do
+registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceClasses' do
    values [{
      name: '1',
      type: :dword,
@@ -82,7 +82,7 @@ end
 
 # Ensure 'Prevent installation of devices using drivers that match these device setup classes: Also apply to matching devices that are already installed.' is set to 'True'
 # BitLocker Rule
-registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrict ions' do
+registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions' do
    values [{
      name: 'DenyDeviceClassesRetroactive',
      type: :dword,
@@ -110,6 +110,7 @@ registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Group Polic
      data: 1,
    }]
    action :create
+   only_if { registry_key_exists?('HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Group Policy\{35378EAC-683F-11D2-A89A-00C04FBBCFA2}', :x86_64)}
 end
 
 # Ensure 'Configure registry policy processing: Process even if the Group Policy objects have not changed' is set to 'Enabled: TRUE'
@@ -120,6 +121,7 @@ registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Group Polic
      data: 1,
    }]
    action :create
+   only_if { registry_key_exists?('HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Group Policy\{35378EAC-683F-11D2-A89A-00C04FBBCFA2}', :x86_64)}
 end
 
 # Ensure 'Turn off background refresh of Group Policy' is set to 'Disabled'
@@ -151,7 +153,7 @@ registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Explorer' d
      data: 1,
    }]
    action :create
-   only_if { node['cis']['level']['2'] }
+   only_if { node['cis']['level']['2'] == "true" }
 end
 
 # Ensure 'Turn off downloading of print drivers over HTTP' is set to 'Enabled'
@@ -163,7 +165,7 @@ registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Printers
      data: 1,
    }]
    action :create
-   only_if { node['cis']['level']['2'] }
+   only_if { node['cis']['level']['2'] == "true" }
 end
 
 # Ensure 'Turn off handwriting personalization data sharing' is set to 'Enabled'
@@ -175,19 +177,19 @@ registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\TabletPC' d
      data: 1,
    }]
    action :create
-   only_if { node['cis']['level']['2'] }
+   only_if { node['cis']['level']['2'] == "true" }
 end
 
 # Ensure 'Turn off handwriting recognition error reporting' is set to 'Enabled'
 # Level 2 Rule
-registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\HandwritingErrorReport s' do
+registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\HandwritingErrorReports' do
    values [{
      name: 'PreventHandwritingErrorReports',
      type: :dword,
      data: 1,
    }]
    action :create
-   only_if { node['cis']['level']['2'] }
+   only_if { node['cis']['level']['2'] == "true" }
 end
 
 # Ensure 'Turn off Internet Connection Wizard if URL connection is referring to Microsoft.com' is set to 'Enabled'
@@ -199,19 +201,19 @@ registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Internet Co
      data: 1,
    }]
    action :create
-   only_if { node['cis']['level']['2'] }
+   only_if { node['cis']['level']['2'] == "true" }
 end
 
 # Ensure 'Turn off Internet download for Web publishing and online ordering wizards' is set to 'Enabled'
 # Level 2 Rule
-registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explore r' do
+registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer' do
    values [{
      name: 'NoWebServices',
      type: :dword,
      data: 1,
    }]
    action :create
-   only_if { node['cis']['level']['2'] }
+   only_if { node['cis']['level']['2'] == "true" }
 end
 
 # Ensure 'Turn off printing over HTTP' is set to 'Enabled'
@@ -223,7 +225,7 @@ registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Printers
      data: 1,
    }]
    action :create
-   only_if { node['cis']['level']['2'] }
+   only_if { node['cis']['level']['2'] == "true" }
 end
 
 # Ensure 'Turn off Registration if URL connection is referring to Microsoft.com' is set to 'Enabled'
@@ -235,7 +237,7 @@ registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Registratio
      data: 1,
    }]
    action :create
-   only_if { node['cis']['level']['2'] }
+   only_if { node['cis']['level']['2'] == "true" }
 end
 
 # Ensure 'Turn off Search Companion content file updates' is set to 'Enabled'
@@ -247,31 +249,31 @@ registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\SearchCompanion' do
      data: 1,
    }]
    action :create
-   only_if { node['cis']['level']['2'] }
+   only_if { node['cis']['level']['2'] == "true" }
 end
 
 # Ensure 'Turn off the Order Prints' picture task' is set to 'Enabled''
 # Level 2 Rule
-registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explore r' do
+registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer' do
    values [{
      name: 'NoOnlinePrintsWizard',
      type: :dword,
      data: 1,
    }]
    action :create
-   only_if { node['cis']['level']['2'] }
+   only_if { node['cis']['level']['2'] == "true" }
 end
 
 # Ensure 'Turn off the Publish to Web' task for files and folders' is set to 'Enabled''
 # Level 2 Rule
-registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explore r' do
+registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer' do
    values [{
      name: 'NoPublishingWizard',
      type: :dword,
      data: 1,
    }]
    action :create
-   only_if { node['cis']['level']['2'] }
+   only_if { node['cis']['level']['2'] == "true" }
 end
 
 # Ensure 'Turn off the Windows Messenger Customer Experience Improvement Program' is set to 'Enabled'
@@ -283,7 +285,7 @@ registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Messenger\Client' d
      data: 1,
    }]
    action :create
-   only_if { node['cis']['level']['2'] }
+   only_if { node['cis']['level']['2'] == "true" }
 end
 
 # Ensure 'Turn off Windows Customer Experience Improvement Program' is set to 'Enabled'
@@ -295,7 +297,7 @@ registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\SQMClient\Windows' 
      data: 1,
    }]
    action :create
-   only_if { node['cis']['level']['2'] }
+   only_if { node['cis']['level']['2'] == "true" }
 end
 
 # Ensure 'Turn off Windows Error Reporting' is set to 'Enabled'
@@ -307,7 +309,7 @@ registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Err
      data: 1,
    }]
    action :create
-   only_if { node['cis']['level']['2'] }
+   only_if { node['cis']['level']['2'] == "true" }
 end
 
 # Ensure 'Support device authentication using certificate' is set to 'Enabled: Automatic'
@@ -319,7 +321,7 @@ registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Polic
      data: 1,
    }]
    action :create
-   only_if { node['cis']['level']['2'] }
+   only_if { node['cis']['level']['2'] == "true" }
 end
 
 # Ensure 'Disallow copying of user input methods to the system account for sign-in' is set to 'Enabled'
@@ -331,7 +333,7 @@ registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Control Panel\Inter
      data: 1,
    }]
    action :create
-   only_if { node['cis']['level']['2'] }
+   only_if { node['cis']['level']['2'] == "true" }
 end
 
 # Ensure 'Do not display network selection UI' is set to 'Enabled'
@@ -418,7 +420,7 @@ end
 
 # Ensure 'Allow standby states (S1-S3) when sleeping (plugged in)' is set to 'Disabled'
 # BitLocker Rule
-registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Power\PowerSettings\abfc25193608-4c2a-94ea-171b0ed546ab' do
+registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Power\PowerSettings\abfc2519-3608-4c2a-94ea-171b0ed546ab' do
    values [{
      name: 'ACSettingIndex',
      type: :dword,
@@ -428,8 +430,10 @@ registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Power\PowerSettings
    only_if { node['cis']['level']['bitlocker'] == "true" }
 end
 
+# Commenting these out for now until I write a check for laptops
+=begin
 # Ensure 'Require a password when a computer wakes (on battery)' is set to 'Enabled'
-registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Power\PowerSettings\0e796bdb100d-47d6-a2d5-f7d2daa51f51' do
+registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Power\PowerSettings\0e796bdb-100d-47d6-a2d5-f7d2daa51f51' do
    values [{
      name: 'DCSettingIndex',
      type: :dword,
@@ -439,7 +443,7 @@ registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Power\PowerSettings
 end
 
 # Ensure 'Require a password when a computer wakes (plugged in)' is set to 'Enabled'
-registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Power\PowerSettings\0e796bdb100d-47d6-a2d5-f7d2daa51f51' do
+registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Power\PowerSettings\0e796bdb-100d-47d6-a2d5-f7d2daa51f51' do
    values [{
      name: 'ACSettingIndex',
      type: :dword,
@@ -449,7 +453,7 @@ registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Power\PowerSettings
 end
 
 # Ensure 'Allow network connectivity during connectedstandby (plugged in)' is set to 'Disabled'
-registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Power\PowerSettings\f15576e898b7-4186-b944-eafa664402d9' do
+registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Power\PowerSettings\f15576e8-98b7-4186-b944-eafa664402d9' do
    values [{
      name: 'ACSettingIndex',
      type: :dword,
@@ -459,7 +463,7 @@ registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Power\PowerSettings
 end
 
 # Ensure 'Allow network connectivity during connectedstandby (on battery)' is set to 'Disabled'
-registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Power\PowerSettings\f15576e898b7-4186-b944-eafa664402d9' do
+registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Power\PowerSettings\f15576e8-98b7-4186-b944-eafa664402d9' do
    values [{
      name: 'DCSettingIndex',
      type: :dword,
@@ -467,6 +471,7 @@ registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Power\PowerSettings
    }]
    action :create
 end
+=end
 
 # Ensure 'Configure Offer Remote Assistance' is set to 'Disabled'
 registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' do
@@ -510,14 +515,14 @@ end
 
 # Ensure 'Microsoft Support Diagnostic Tool: Turn on MSDT interactive communication with support provider' is set to 'Disabled'
 # Level 2 Rule
-registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\ScriptedDiagnosticsPro vider\Policy' do
+registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\ScriptedDiagnosticsProvider\Policy' do
    values [{
      name: 'DisableQueryRemoteServer',
      type: :dword,
      data: 1,
    }]
    action :create
-   only_if { node['cis']['level']['2'] }
+   only_if { node['cis']['level']['2'] == "true" }
 end
 
 # Ensure 'Enable/Disable PerfTrack' is set to 'Disabled'
@@ -529,7 +534,7 @@ registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WDI\{9c5a40
      data: 1,
    }]
    action :create
-   only_if { node['cis']['level']['2'] }
+   only_if { node['cis']['level']['2'] == "true" }
 end
 
 # Ensure 'Turn off the advertising ID' is set to 'Enabled'
@@ -541,29 +546,29 @@ registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\policies\Microsoft\Windows\Advertising
      data: 1,
    }]
    action :create
-   only_if { node['cis']['level']['2'] }
+   only_if { node['cis']['level']['2'] == "true" }
 end
 
 # Ensure 'Enable Windows NTP Client' is set to 'Enabled'
 # Level 2 Rule
-registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\W32Time\TimeProviders\NtpClien t' do
+registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\W32Time\TimeProviders\NtpClient' do
    values [{
      name: 'Enabled',
      type: :dword,
      data: 1,
    }]
    action :create
-   only_if { node['cis']['level']['2'] }
+   only_if { node['cis']['level']['2'] == "true" }
 end
 
 # Ensure 'Enable Windows NTP Server' is set to 'Disabled'
 # Level 2 Rule
-registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\W32Time\TimeProviders\NtpServe r' do
+registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\W32Time\TimeProviders\NtpServer' do
    values [{
      name: 'Enabled',
      type: :dword,
      data: 1,
    }]
    action :create
-   only_if { node['cis']['level']['2'] }
+   only_if { node['cis']['level']['2'] == "true" }
 end
