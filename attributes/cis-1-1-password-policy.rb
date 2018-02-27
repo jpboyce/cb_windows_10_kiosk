d2bd42f66ce47cd3f89d5cfe6164ebdba5e94b8c
Jesse Boyce <jesse@jpboyce.org>
2018-02-27 20:55:25 +1000

    Updated attributes to a more standardised naem

--- attributes/cis-1-1-password-policy.rb
+++ attributes/cis-1-1-password-policy.rb
@@ -1,19 +1,19 @@
 # Password Policy settings
 
 # 1.1.1 (L1) Ensure 'Enforce password history' is set to '24 or more password(s)'
-default['cfg_file']['system_access']['PasswordHistorySize'] = nil
+default['security_policy']['system_access']['PasswordHistorySize'] = nil
 
 # 1.1.2 (L1) Ensure 'Maximum password age' is set to '60 or fewer days, but not 0'
-default['cfg_file']['system_access']['MaximumPasswordAge'] = 60
+default['security_policy']['system_access']['MaximumPasswordAge'] = 60
 
 # 1.1.3 (L1) Ensure 'Minimum password age' is set to '1 or more day(s)'
-default['cfg_file']['system_access']['MinimumPasswordAge'] = 1
+default['security_policy']['system_access']['MinimumPasswordAge'] = 1
 
 # 1.1.4 (L1) Ensure 'Minimum password length' is set to '14 or more character(s)'
-default['cfg_file']['system_access']['MinimumPasswordLength'] = 15
+default['security_policy']['system_access']['MinimumPasswordLength'] = 15
 
 # 1.1.5 (L1) Ensure 'Password must meet complexity requirements' is set to 'Enabled'
-default['cfg_file']['system_access']['PasswordComplexity'] = 1
+default['security_policy']['system_access']['PasswordComplexity'] = 1
 
 # 1.1.6 (L1) Ensure 'Store passwords using reversible encryption' is set to 'Disabled'
-default['cfg_file']['system_access']['ClearTextPassword'] = 0
+default['security_policy']['system_access']['ClearTextPassword'] = 0
