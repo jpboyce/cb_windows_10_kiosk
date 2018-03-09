7b406946407b68dc0bed231e5be187e3abc4f4fa
Jesse Boyce <jesse@jpboyce.org>
2018-03-09 23:48:08 +1000

    put commented out rule template in for now

--- recipes/custom-windows-firewall-rules.rb
+++ recipes/custom-windows-firewall-rules.rb
@@ -1,18 +1,21 @@
 # Custom Windows Firewall Rules
 # Rules to apply once the CIS rules are applied
 
-# Rules should have the following items:
-# Name
-# DisplayName
-# Description
-# Group
-# Profile (Any/Domain/Private/Public/NotApplicable)
-# Direction (Inbound/Outbound)
-# Action (Allow/Block)
-# LocalAddress
-# RemoteAddress
-# Protocol
-# LocalPort
-# RemotePort
-# Program
-# Service
+=begin
+# Allow Ping
+  windows_firwall_rule 'Allow ICMP Inbound' do
+  name 'Allow ICMP Inbound'
+  description 'Standard Rule by Chef - Allow ICMP Inbound'
+  localip 'any'
+  localport 'any'
+  remoteip 'any'
+  remoteport 'any'
+  dir in
+  protocol 'icmpv4'
+  firewall_action 'allow'
+  profile 'any'
+  program 'any'
+  service 'any'
+  interface_type 'any'
+end
+=end
