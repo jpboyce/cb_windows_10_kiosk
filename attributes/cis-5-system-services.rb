f4ce16c11f661db0051e656598580d4b74799227
Jesse Boyce <jesse@jpboyce.org>
2018-03-11 21:57:13 +1000

    cookstyle fixes

--- attributes/cis-5-system-services.rb
+++ attributes/cis-5-system-services.rb
@@ -1,7 +1,7 @@
 # CIS 5 - System Services
 
 # Level 1 services to disable
-default['cis-5']['level1']['services'] = ['Browser', 'HomeGroupListener', 'HomeGroupProvider', 'IISADMIN', 'irmon', 'SharedAccess', 'LxssManager', 'FTPSVC', 'RpcLocator', 'RemoteAccess', 'simptcp', 'SSDPSRV', 'upnphost', 'WMSvc', 'WMPNetworkSvc', 'icssvc', 'WinHttpAutoProxySvc', 'W3SVC', 'XboxGipSvc', 'xbgm', 'XblAuthManager', 'XblGameSave', 'XboxNetApiSvc']
+default['cis-5']['level1']['services'] = %w(Browser HomeGroupListener HomeGroupProvider IISADMIN irmon SharedAccess LxssManager FTPSVC RpcLocator RemoteAccess simptcp SSDPSRV upnphost WMSvc WMPNetworkSvc icssvc WinHttpAutoProxySvc W3SVC XboxGipSvc xbgm XblAuthManager XblGameSave XboxNetApiSvc)
 
 # Level 2 services to disable
-default['cis-5']['level2']['services'] = ['BthHFSrv', 'bthserv', 'MapsBroker', 'lfsvc', 'lltdsvc', 'MSiSCSI', 'PNRPsvc', 'p2psvc', 'p2pimsvc', 'PNRPAutoReg', 'wercplsupport', 'RasAuto', 'SessionEnv', 'TermService', 'UmRdpService', 'RemoteRegistry', 'LanmanServer', 'SNMP', 'WerSvc', 'Wecsvc', 'WpnService', 'WinRM']
+default['cis-5']['level2']['services'] = %w(BthHFSrv bthserv MapsBroker lfsvc lltdsvc MSiSCSI PNRPsvc p2psvc p2pimsvc PNRPAutoReg wercplsupport RasAuto SessionEnv TermService UmRdpService RemoteRegistry LanmanServer SNMP WerSvc Wecsvc WpnService WinRM)
