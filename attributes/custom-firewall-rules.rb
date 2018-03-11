2d40b000062bb4c573de785d67579d55b06a233e
Jesse Boyce <jesse@jpboyce.org>
2018-03-11 22:28:30 +1000

    cookstyle fixes

--- attributes/custom-firewall-rules.rb
+++ attributes/custom-firewall-rules.rb
@@ -1,18 +1,16 @@
 # Custom firewall rules
 # Put your custom firewall rules in for your Kiosk here, layout is below
 
-=begin
-default['firewallrule']['placeholder']['name'] = 'Placeholder Rule'
-default['firewallrule']['placeholder']['description'] = 'Placeholder Rule'
-default['firewallrule']['placeholder']['localip'] = 'any'
-default['firewallrule']['placeholder']['localport'] = 'any'
-default['firewallrule']['placeholder']['remoteip'] = 'any'
-default['firewallrule']['placeholder']['remoteport'] = 'any'
-default['firewallrule']['placeholder']['dir'] = 'in'
-default['firewallrule']['placeholder']['protocol'] = 'tcp'
-default['firewallrule']['placeholder']['firewall_action'] = 'allow'
-default['firewallrule']['placeholder']['profile'] = 'any'
-default['firewallrule']['placeholder']['program'] = 'System'
-default['firewallrule']['placeholder']['service'] = ''
-default['firewallrule']['placeholder']['interface_type'] = 'any'
-=end
+# default['firewallrule']['placeholder']['name'] = 'Placeholder Rule'
+# default['firewallrule']['placeholder']['description'] = 'Placeholder Rule'
+# default['firewallrule']['placeholder']['localip'] = 'any'
+# default['firewallrule']['placeholder']['localport'] = 'any'
+# default['firewallrule']['placeholder']['remoteip'] = 'any'
+# default['firewallrule']['placeholder']['remoteport'] = 'any'
+# default['firewallrule']['placeholder']['dir'] = 'in'
+# default['firewallrule']['placeholder']['protocol'] = 'tcp'
+# default['firewallrule']['placeholder']['firewall_action'] = 'allow'
+# default['firewallrule']['placeholder']['profile'] = 'any'
+# default['firewallrule']['placeholder']['program'] = 'System'
+# default['firewallrule']['placeholder']['service'] = ''
+# default['firewallrule']['placeholder']['interface_type'] = 'any'
--- resources/secedit.rb
+++ resources/secedit.rb
@@ -1,24 +1,25 @@
-# Secedit resource for doing actions via the secedit.exe program
-
-resource_name :secedit
-property :template, String, required: false, default: ''
-property :database, String, required: false, default: ''
-property :log, String, default: ''
-
-action :configure do
-    if node['platform'] == 'windows'
-        template "#{template}" do
-            source ''
-            cookbook 'cb-windows-10-kiosk'
-            action :create
-        end
-
-        execute 'Configure security database' do
-          command "secedit.exe /configure /db #{database} /cfg #{template} /log #{log}"
-          live_stream true
-          action :run
-        end
-    else
-        Chef::Log.info "#{@current_resource} is only for a Windows platform"
-    end
-end
+# # Secedit resource for doing actions via the secedit.exe program
+#
+# resource_name :secedit
+# property :template, String, required: false, default: ''
+# property :database, String, required: false, default: ''
+# property :log, String, default: ''
+#
+# action :configure do
+#     if node['platform'] == 'windows'
+#         template "#{template}" do
+#             source ''
+#             cookbook 'cb-windows-10-kiosk'
+#             action :create
+#         end
+#
+#         execute 'Configure security database' do
+#           command "secedit.exe /configure /db #{database} /cfg #{template} /log #{log}"
+#           live_stream true
+#           action :run
+#         end
+#     else
+#         Chef::Log.info "#{@current_resource} is only for a Windows platform"
+#     end
+# end
+#
