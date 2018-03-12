44edd5d894a60642326674cfd65385f7404c5782
Jesse Boyce <jesse@jpboyce.org>
2018-03-12 22:34:33 +1000

    updated cookbook name references that had hypens to underscores

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
-name 'cb-windows-10-kiosk'
+name 'cb_windows_10_kiosk'
 maintainer 'Jesse Boyce'
 maintainer_email 'jesse@jpboyce.org'
 license 'MIT'
-description 'Installs/Configures cb-windows-10-kiosk'
+description 'Installs/Configures cb_windows_10_kiosk'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
@@ -11,13 +11,13 @@ chef_version '>= 12.1' if respond_to?(:chef_version)
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-issues_url 'https://github.com/jpboyce/cb-windows-10-kiosk/issues'
+issues_url 'https://github.com/jpboyce/cb_windows_10_kiosk/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-source_url 'https://github.com/jpboyce/cb-windows-10-kiosk'
+source_url 'https://github.com/jpboyce/cb_windows_10_kiosk'
 supports 'windows'
 depends 'windows', '>= 3.0'
 depends 'windows-security-policy'
