5ee07d8f6fc0b40f3f07c28bb95d5bb1b1c3c570
Jesse Boyce <jesse@jpboyce.org>
2018-03-14 08:14:34 +1000

    changed values to integers

--- attributes/default.rb
+++ attributes/default.rb
@@ -29,25 +29,40 @@ default['account']['kiosk']['shell'] = 'explorer.exe'
 default['power_setting']['active_plan']['name'] = 'High Performance'
 default['power_setting']['active_plan']['guid'] = '8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c'
 # Never turn off the hard disk
-default['power_setting']['active_plan']['disk_timeout'] = '0'
+default['power_setting']['active_plan']['disk_timeout'] = 0
 # IE Javascript performance to max
-default['power_setting']['active_plan']['ie_javascript_timer'] = '1'
+default['power_setting']['active_plan']['ie_javascript_timer'] = 1
 # Sleep after 0 minutes (never)
-default['power_setting']['active_plan']['standby_timeout'] = '0'
+default['power_setting']['active_plan']['standby_timeout'] = 0
 # Allow hybrid sleep
 # Hibernate after 0 minutes (never)
-default['power_setting']['active_plan']['hibernate_timeout'] = '0'
+default['power_setting']['active_plan']['hibernate_timeout'] = 0
 # USB selective suspend setting, 0 to disable
-default['power_setting']['active_plan']['usb_suspend'] = '0'
+default['power_setting']['active_plan']['usb_suspend'] = 0
 # Power button setting, 3=Shutdown
-default['power_setting']['active_plan']['power_button'] = '3'
+default['power_setting']['active_plan']['power_button'] = 3
 # PCI Express Power, 0 = power management off
-default['power_setting']['active_plan']['pci_power'] = '0'
+default['power_setting']['active_plan']['pci_power'] = 0
 # CPU Power management for min performance, 64=max performance
-default['power_setting']['active_plan']['min_cpu_power'] = '64'
+default['power_setting']['active_plan']['min_cpu_power'] = 64
 # CPU Power management for max performance, 64=max performance
-default['power_setting']['active_plan']['max_cpu_power'] = '64'
+default['power_setting']['active_plan']['max_cpu_power'] = 64
 # System Cooling Policy, 1=Active
-default['power_setting']['active_plan']['cooling_policy'] = '1'
+default['power_setting']['active_plan']['cooling_policy'] = 1
 # Turn off monitor, 1200 seconds or 20 minutes
-default['power_setting']['active_plan']['monitor_timeout'] = '1200'
+default['power_setting']['active_plan']['monitor_timeout'] = 1200
+
+# Keyboard Filter items
+default['keyboard_filter']['blocked_keys'] = [
+'Win+E',
+'Win+F',
+'Win+R',
+'Alt+Tab',
+'Ctrl+Tab',
+'Win+Tab',
+'Win+D',
+'Win+M',
+'Shift+Ctrl+Esc',
+'Win+L',
+'Win+U'
+]
