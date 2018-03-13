c3dec331e61f6f87a4797c7b6bf34546f29c0404
Jesse Boyce <jesse@jpboyce.org>
2018-03-13 21:19:57 +1000

    fixed typo in node attribute name

--- recipes/custom-power-settings.rb
+++ recipes/custom-power-settings.rb
@@ -1,24 +1,24 @@
 # Custom Power Settings
 # Monitor timeout
-execute 'monitor-timeout-ac' do
-  command "powercfg /X monitor-timeout-ac #{node['power_settings']['active_plan']['monitor_timeout']}"
+execute 'monitorTimeout' do
+  command "powercfg /X monitor-timeout-ac #{node['power_setting']['active_plan']['monitor_timeout']}"
   action :run
 end
 
 # Disk Timeout
-execute 'disk-timeout-ac' do
-  command "powercfg /X disk-timeout-ac #{node['power_settings']['active_plan']['disk_timeout']}"
+execute 'diskTimeout' do
+  command "powercfg /X disk-timeout-ac #{node['power_setting']['active_plan']['disk_timeout']}"
   action :run
 end
 
 # Standby Timeout
-execute 'standby-timeout-ac' do
-  command "powercfg /X standby-timeout-ac #{node['power_settings']['active_plan']['standby_timeout']}"
+execute 'standbyTimeout' do
+  command "powercfg /X standby-timeout-ac #{node['power_setting']['active_plan']['standby_timeout']}"
   action :run
 end
 
 # Hibernate Timeout
-execute 'hibernate-timeout-ac' do
-  command "powercfg /X hibernate-timeout-ac #{node['power_settings']['active_plan']['hibernate_timeout']}"
+execute 'hibernateTimeout' do
+  command "powercfg /X hibernate-timeout-ac #{node['power_setting']['active_plan']['hibernate_timeout']}"
   action :run
 end
