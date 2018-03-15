7060f9cdbea542fdf30d39c213a578dd9b0c9d71
Jesse Boyce <jesse@jpboyce.org>
2018-03-16 07:35:20 +1000

    Updated attribute references to new format

--- recipes/custom-power-settings.rb
+++ recipes/custom-power-settings.rb
@@ -1,24 +1,24 @@
 # Custom Power Settings
 # Monitor timeout
 execute 'monitorTimeout' do
-  command "powercfg /X monitor-timeout-ac #{node['power_setting']['active_plan']['monitor_timeout']}"
+  command "powercfg /X monitor-timeout-ac #{node['power_setting']['monitor_timeout']}"
   action :run
 end
 
 # Disk Timeout
 execute 'diskTimeout' do
-  command "powercfg /X disk-timeout-ac #{node['power_setting']['active_plan']['disk_timeout']}"
+  command "powercfg /X disk-timeout-ac #{node['power_setting']['disk_timeout']}"
   action :run
 end
 
 # Standby Timeout
 execute 'standbyTimeout' do
-  command "powercfg /X standby-timeout-ac #{node['power_setting']['active_plan']['standby_timeout']}"
+  command "powercfg /X standby-timeout-ac #{node['power_setting']['standby_timeout']}"
   action :run
 end
 
 # Hibernate Timeout
 execute 'hibernateTimeout' do
-  command "powercfg /X hibernate-timeout-ac #{node['power_setting']['active_plan']['hibernate_timeout']}"
+  command "powercfg /X hibernate-timeout-ac #{node['power_setting']['hibernate_timeout']}"
   action :run
 end
--- recipes/local-accounts.rb
+++ recipes/local-accounts.rb
@@ -3,16 +3,16 @@
 # Create account
 user 'kiosk' do
     action :create
-    username node['account']['kiosk']['username']
-    password node['account']['kiosk']['password']
-    comment node['account']['kiosk']['description']
+    username node['cb_windows_10_kiosk']['account']['username']
+    password node['cb_windows_10_kiosk']['account']['password']
+    comment node['cb_windows_10_kiosk']['account']['description']
 end
 
 # Add to defined groups
-node['account']['kiosk']['groups'].each do |i|
+node['cb_windows_10_kiosk']['account']['groups'].each do |i|
     group i do
         action :modify
-        members node['account']['kiosk']['username']
+        members node['cb_windows_10_kiosk']['account']['username']
         append true
     end
 end
@@ -20,13 +20,13 @@ end
 # Set password never expires
 # NET USER <user> /EXPIRES:NEVER
 execute 'password_never_expires' do
-    command "NET USER #{node['account']['kiosk']['username']} /EXPIRES:NEVER"
+    command "NET USER #{node['cb_windows_10_kiosk']['account']['username']} /EXPIRES:NEVER"
     action :run
 end
 
 # set user can't change password
 # NET USER <user> /PASSWORDCHG:NO
 execute 'user_cant_change_password' do
-    command "NET USER #{node['account']['kiosk']['username']} /PASSWORDCHG:NO"
+    command "NET USER #{node['cb_windows_10_kiosk']['account']['username']} /PASSWORDCHG:NO"
     action :run
 end
