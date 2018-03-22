ce4fb23eb63182d431f5937b43455828a96bb8ac
Jesse Boyce <jesse@jpboyce.org>
2018-03-22 20:02:02 +1000

    Updated to new attribute name format

--- attributes/cis-5-system-services.rb
+++ attributes/cis-5-system-services.rb
@@ -1,7 +1,7 @@
 # CIS 5 - System Services
 
 # Level 1 services to disable
-default['cis-5']['level1']['services'] = %w(Browser HomeGroupListener HomeGroupProvider IISADMIN irmon SharedAccess LxssManager FTPSVC RpcLocator RemoteAccess simptcp SSDPSRV upnphost WMSvc WMPNetworkSvc icssvc WinHttpAutoProxySvc W3SVC XboxGipSvc xbgm XblAuthManager XblGameSave XboxNetApiSvc)
+default['cb_windows_10_kiosk']['cis-level-1']['services'] = %w(Browser HomeGroupListener HomeGroupProvider IISADMIN irmon SharedAccess LxssManager FTPSVC RpcLocator RemoteAccess simptcp SSDPSRV upnphost WMSvc WMPNetworkSvc icssvc WinHttpAutoProxySvc W3SVC XboxGipSvc xbgm XblAuthManager XblGameSave XboxNetApiSvc)
 
 # Level 2 services to disable
-default['cis-5']['level2']['services'] = %w(BthHFSrv bthserv MapsBroker lfsvc lltdsvc MSiSCSI PNRPsvc p2psvc p2pimsvc PNRPAutoReg wercplsupport RasAuto SessionEnv TermService UmRdpService RemoteRegistry LanmanServer SNMP WerSvc Wecsvc WpnService WinRM)
+default['cb_windows_10_kiosk']['cis-level-2']['services'] = %w(BthHFSrv bthserv MapsBroker lfsvc lltdsvc MSiSCSI PNRPsvc p2psvc p2pimsvc PNRPAutoReg wercplsupport RasAuto SessionEnv TermService UmRdpService RemoteRegistry LanmanServer SNMP WerSvc Wecsvc WpnService WinRM)
